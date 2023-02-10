bcf9c0ef3b5c47ff276d20c57352fb3be26e2443
xfafnir <xfafnir@ol-rogue>
2023-02-10 17:27:57 -0600

    updated error handles

--- tarfunc.go
+++ tarfunc.go
@@ -14,12 +14,12 @@ func tarGzipArchive(workingDir *string, action_targz *string) {
 	fpf(os.Stdout, "Creating zip archive... %s\n", (*workingDir + "/" + *action_targz))
 	fileNames, err := os.ReadDir(*workingDir)
 	if err != nil {
-		log.Fatal(err)
+		lff("Tar: fileNames: ReadDi(): failed: %w", err.Error())
 	}
 
 	archive, err := os.Create(*workingDir + "/" + *action_targz)
 	if err != nil {
-		log.Fatal(err)
+		lff("Tar: archive: Create(): failed: %w", err.Error())
 	}
 	defer archive.Close()
 
@@ -31,31 +31,32 @@ func tarGzipArchive(workingDir *string, action_targz *string) {
 
 	for _, file := range fileNames {
 		fpf(os.Stdout, "Archiving file.... %s\n", file)
-		if file.IsDir() {
-		}
+		//if file.IsDir() {
+		//}
 		files, err := os.Open(file.Name())
 		if err != nil {
-			log.Fatal(err)
+			lff("Tar: files: Open(): failed: %w", err.Error())
 		}
 		defer files.Close()
 		info, err := files.Stat()
 		if err != nil {
-			log.Fatal(err)
+			lff("Tar: info: files.Stat(): failed: %w", err.Error())
 		}
 
 		header, err := tar.FileInfoHeader(info, info.Name())
 		if err != nil {
-			log.Fatal(err)
+			lff("Tar: header: tar.FileInfoHeader(): failed: %w", err.Error())
 		}
 		header.Name = file.Name()
 
 		err = tarWriter.WriteHeader(header)
 		if err != nil {
-			log.Fatal(err)
+			lff("Tar: tarWriter: WriteHeader(): failed: %w", err.Error())
 		}
 
 		if _, err := io.Copy(tarWriter, files); err != nil {
 			log.Fatal(err)
+			lff("Tar: Last: Copy(): failed: %w", err.Error())
 		}
 	}
 
@@ -66,18 +67,18 @@ func utarGzipArchive(workingDir *string, action_utargz *string) {
 	//myReader, err := tar.OpenReader(*action_utargz)
 	myReader, err := os.Open(*action_utargz)
 	if err != nil {
-		log.Fatal("openreader failed")
+		lff("Untar: myReader: Open(): failed: %w", err.Error())
 	}
 	ugziper, err := gzip.NewReader(myReader)
 	// ugziper, err := gzip.NewReader(io.Reader(*action_utargz))
 	if err != nil {
-		lff("ugziper: NewReader(): failed %w", err.Error())
+		lff("Untar: ugziper: NewReader(): failed %w", err.Error())
 	}
 	defer ugziper.Close()
 
 	untargz := tar.NewReader(ugziper)
 	if err != nil {
-		lff("untargz: NewReader(): failed %w", err.Error())
+		lff("Untar: untargz: NewReader(): failed %w", err.Error())
 	}
 	// Open and iterate through the files in the archive.
 	for {
@@ -86,26 +87,29 @@ func utarGzipArchive(workingDir *string, action_utargz *string) {
 			break // End of archive
 		}
 		if err != nil {
-			lff("err not nil %w", err.Error())
+			lff("Untar: header: untargz.Next(): failed: %w", err.Error())
 		}
 		filePath := filepath.Join(strings.TrimSuffix(*action_utargz, filepath.Ext(*action_utargz)), header.Name)
 		if header.FileInfo().IsDir() {
 			//pl("creating directory...")
 			if err := os.MkdirAll(filePath, os.ModePerm); err != nil {
 				log.Fatal(err)
+				lff("Untar: header.Fileinfo: MkdirAll(): failed: %w", err.Error())
 			}
 			continue
 		}
 		if err := os.MkdirAll(filepath.Dir(filePath), os.ModePerm); err != nil {
 			log.Fatal(err)
+			lff("Untar: filepath.Dir(filePath): MkdirAll(): failed: %w", err.Error())
 		}
 		destFile, err := os.OpenFile(filePath, os.O_WRONLY|os.O_CREATE|os.O_TRUNC, header.FileInfo().Mode())
 		if err != nil {
 			log.Fatal(err)
+			lff("Untar: destFile: OpenFile(): failed: %w", err.Error())
 		}
 
 		if _, err := io.Copy(destFile, untargz); err != nil {
-			lff("Last: Copy(): failed %w", err.Error())
+			lff("Untar: Last: Copy(): failed: %w", err.Error())
 		}
 
 		destFile.Close()
